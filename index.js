cb4baae23fd596d342215863aab25a0b20afdb96
Uuganbaatar <uuganbaatarbaynsuren2016@gmail.com>
2019-06-03 20:25:44 +0800

    Fast indexing by using bulk import

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 /* eslint-disable no-console */
-const request = require('request');
+const request = require('request-promise-native');
 const Typesense = require('typesense');
+const fs = require('fs');
+
+const HOST = '127.0.0.1';
+const PORT = '8108';
+const PROTOCOL = 'http';
+const API_KEY = 'KONO';
 
 const client = new Typesense.Client({
   'masterNode': {
-    'host': '127.0.0.1',
-    'port': '8108',
-    'protocol': 'http',
-    'apiKey': 'KONO'
+    'host': HOST,
+    'port': PORT,
+    'protocol': PROTOCOL,
+    'apiKey': API_KEY
   },
   'timeoutSeconds': 2
 });
@@ -51,34 +57,52 @@ const schema = {
     }
   }
 
-  const url = 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json';
-  
-  request(url, async (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const resJson = JSON.parse(body);
-      
-      for (let i = 0; i < resJson.applist.apps.length; i++) {
-        const element = resJson.applist.apps[i];
+  const steamOptions = {
+    url: 'http://api.steampowered.com/ISteamApps/GetAppList/v0002/?format=json',
+    json: true
+  }
 
-        try {
-          await client.collections('games').documents().create({
-            'id': element.appid.toString(),
-            'appid': element.appid,
-            'name': element.name,
-            'image': `https://steamcdn-a.akamaihd.net/steam/apps/${element.appid}/header.jpg`,
-            'steam_url': `https://store.steampowered.com/app/${element.appid}/`
-          });
-        } catch (err) {
-          // 409 = Already exists
-          if (!err.message.includes('409')) {
-            console.log(err);
-          }
-        }
-      }
+  let steamJson;
+  try {
+    steamJson = await request(steamOptions);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
 
-      const collections = await client.collections().retrieve();
-      console.log(collections);  
+  // Convert all games to JSONL format for bulk indexing
+  let bulkDocuments = '';
+  for (let i = 0; i < steamJson.applist.apps.length; i++) {
+    const element = steamJson.applist.apps[i];
+    bulkDocuments += JSON.stringify({
+      'id': element.appid.toString(),
+      'appid': element.appid,
+      'name': element.name,
+      'image': `https://steamcdn-a.akamaihd.net/steam/apps/${element.appid}/header.jpg`,
+      'steam_url': `https://store.steampowered.com/app/${element.appid}/`
+    });
+    bulkDocuments += '\n';
+  }
+
+  if (bulkDocuments === '') return;
+
+  const bulkOptions = {
+    method: 'POST',
+    body: bulkDocuments,
+    url: `${PROTOCOL}://${HOST}:${PORT}/collections/games/documents/import`,
+    headers: {
+      'X-TYPESENSE-API-KEY': API_KEY
     }
-  });
+  }
+
+  try {
+    const bulkResponse = await request(bulkOptions);
+    fs.writeFileSync('typesense_import_response.txt', bulkResponse);
+  } catch (err) {
+    console.log(err);
+  }
+
+  const collections = await client.collections().retrieve();
+  console.log(collections);
 
 })();
